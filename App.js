41ff928b3044fcfbeadcb70056f225dc215c5d13
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-20 00:50:41 +0600

    image, state, input, text added

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text style={{fontSize:'20px'}}>Welcome <span style={{color:'tomato'}}> Raufu Prezens</span>!</Text> */}
+      <Text>Welcome Raufu Prezens!</Text>
+      <Text style={{fontSize:40}}>{name}</Text>
+      <Image
+        source={{
+          uri: 'https://raufuprezens.files.wordpress.com/2020/04/a-1.jpg?w=683',
+        }}
+        style={{ width: 200, height: 300 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        onChangeText = {text=>setName(text)}
+        defaultValue="Your Name"
+      />
       <StatusBar style="auto" />
     </View>
   );
